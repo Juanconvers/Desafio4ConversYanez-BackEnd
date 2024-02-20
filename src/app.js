19b488375726bf8fc8611710c0b4984726b48414
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-02-20 12:45:09 -0500

    Commit No. 3: Implementando Handlebars

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
  
 import express from "express";
 import productsRouter from './routes/ProductsRouter.js';
-import { __dirname } from "./path.js";
 import upload from "./config/multer.js";
 import cartRouter from "./routes/cartRouter.js";
+import { engine } from 'express-handlebars'
+import { __dirname } from "./path.js";
 
 console.log(__dirname)
 
 const app = express()
 const PORT = 11000
 
+//Middlewares
 app.use(express.json())
 app.use('/static', express.static(__dirname + '/public'))
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
 
 
+//Routes
 app.use('/api/products', productsRouter)
 app.use('/api/cart', cartRouter)
 app.post('/upload', upload.single('product'), (req, res) => {
@@ -24,7 +30,13 @@ app.post('/upload', upload.single('product'), (req, res) => {
         res.status(500).send("Error al subir imagen")
     }
 }) 
+app.get('/static', (req, res) => {
+    res.render('home')
+})
 
+
+
+//Server
 app.listen(PORT,() => {
     console.log(`Server on port ${PORT}`)
 })
